ff76f7dd9934b0e528eed2cf67d972c3979dcc19
k.ioannou <132226067+IoannouKon@users.noreply.github.com>
2024-05-03 20:34:37 +0300

    add some dffs

--- VHDL_exercises/VLSI-5(FIR_in_Zybo)/FIR_Correct.vhdl
+++ VHDL_exercises/VLSI-5(FIR_in_Zybo)/FIR_Correct.vhdl
@@ -12,7 +12,17 @@ entity dff is
       rst : in std_logic
      );
 end entity;
+architecture Behavioral of dff is
 
+begin
+    process(clk, rst) begin
+        if rst='1' then
+            q<='0';
+        elsif rising_edge(clk) then
+            q<=d;
+        end if;
+    end process;
+end Behavioral;
 
 --DFFinput
 library IEEE;
